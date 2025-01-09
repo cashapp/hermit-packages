1586d806c1140bfdad70068652d10a3205a803b2
Sutina Wipawiwat <swipawiwat@squareup.com>
2025-01-09 12:27:44 +1100

    Remove unneeded --add-opens

--- kotlin-format/kotlin-format.sh
+++ kotlin-format/kotlin-format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 KOTLIN_FORMAT_DIR="$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")"
-exec java --add-opens java.base/java.lang=ALL-UNNAMED -Xmx512m -jar "${KOTLIN_FORMAT_DIR}/kotlin-format.jar" "$@"
+exec java -Xmx512m -jar "${KOTLIN_FORMAT_DIR}/kotlin-format.jar" "$@"
