cashapp/hermit-packages

commit b4ca316e5742acbc778245a43812a4e7df787e1f

Auto-versioned: act.hcl awscli.hcl buildkite-agent.hcl conftest.hcl copybara.hcl depot.hcl duckdb.hcl flyctl.hcl fossa.hcl gauge.hcl gitversion.hcl gren.hcl just.hcl kubectx.hcl kubens.hcl meilisearch.hcl please.hcl prek.hcl rclone.hcl snyk.hcl stripe.hcl uv.hcl zellij.hcl

zellij.hcl
@@ -12,7 +12,7 @@ darwin {
   sha256-source = "https://github.com/zellij-org/zellij/releases/download/v${version}/zellij-${xarch}-apple-darwin.sha256sum"
 }
 
-version "0.43.1" {
+version "0.43.1" "0.44.0" {
   auto-version {
     github-release = "zellij-org/zellij"
   }
@@ -23,4 +23,8 @@ sha256sums = {
   "https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-x86_64-apple-darwin.tar.gz": "7b0c1a9c2591eadf506ec58d62ef5f6d9c93d089a3603142af3dcca5fa575d44",
   "https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-aarch64-apple-darwin.tar.gz": "a09ea51f3d98427253de2b889bb04f1aa0850fbb034911c7a01b8f0194edba66",
   "https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-no-web-aarch64-unknown-linux-musl.tar.gz": "8ced877df27a8fe9112607dd3d772442aefa5e42359cda1baba53e78c4ae46aa",
+  "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-no-web-x86_64-unknown-linux-musl.tar.gz": "d7239c8f8c08dc7bb73920fa7757d776a81f899a45edfc1d0c862a0368db7127",
+  "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-x86_64-apple-darwin.tar.gz": "aa95a65c99ac9b9e609411ca0897f3778d2cd8a8363c71b61b68101758532b14",
+  "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-aarch64-apple-darwin.tar.gz": "89a9273955c64bfafa1325e227a6c3bb3b81c2648b5999a69d57f6728933b1b8",
+  "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-no-web-aarch64-unknown-linux-musl.tar.gz": "4b2d47a210dcaabde40e659bfb00677d2dccce2238e562a145fe8df68a35b6c4",
 }

act.hcl
@@ -16,7 +16,7 @@ version "0.2.45" "0.2.46" "0.2.48" "0.2.49" "0.2.50" "0.2.51" "0.2.52" "0.2.53"
         "0.2.54" "0.2.55" "0.2.56" "0.2.57" "0.2.58" "0.2.59" "0.2.60" "0.2.61" "0.2.62"
         "0.2.63" "0.2.64" "0.2.65" "0.2.66" "0.2.67" "0.2.68" "0.2.69" "0.2.70" "0.2.71"
         "0.2.72" "0.2.73" "0.2.74" "0.2.75" "0.2.76" "0.2.77" "0.2.78" "0.2.79" "0.2.80"
-        "0.2.81" "0.2.82" "0.2.83" "0.2.84" {
+        "0.2.81" "0.2.82" "0.2.83" "0.2.84" "0.2.85" {
   auto-version {
     github-release = "nektos/act"
   }
@@ -179,4 +179,8 @@ sha256sums = {
   "https://github.com/nektos/act/releases/download/v0.2.84/act_linux_x86_64.tar.gz": "19d4525fb0d80ff50bd3252d3fa47ee1265a331fe4c39249ac65347e527e16e2",
   "https://github.com/nektos/act/releases/download/v0.2.84/act_darwin_x86_64.tar.gz": "46f6f5229a76ebf39fa51dd9ea05cb67db5b05355af204af595553b6d8cee618",
   "https://github.com/nektos/act/releases/download/v0.2.84/act_linux_arm64.tar.gz": "116c7f27c4862fb01730c956c9bc9f51f9d44d8f3850fe6166b7da9f68677a5c",
+  "https://github.com/nektos/act/releases/download/v0.2.85/act_linux_arm64.tar.gz": "670cf0746b2b0a66f40202f76c28a573da000c043ef56d924439c26d71f40fde",
+  "https://github.com/nektos/act/releases/download/v0.2.85/act_darwin_x86_64.tar.gz": "d5dfc26a5d2f0b089fb8d08a3d0eaf2b24863456b6f93a468ffb246b75c96d31",
+  "https://github.com/nektos/act/releases/download/v0.2.85/act_darwin_arm64.tar.gz": "d56bb949b68dd69807da4dc1cb451c704b397aa0afb25793b1080d4f8478bbd7",
+  "https://github.com/nektos/act/releases/download/v0.2.85/act_linux_x86_64.tar.gz": "fbf411fb5a47bff407d3be1f320f3314af050d92b0e7e9615075222ba0c22947",
 }

awscli.hcl
@@ -53,7 +53,7 @@ version "2.0.40" "2.1.26" "2.2.4" "2.4.2" "2.4.4" "2.4.7" "2.5.8" "2.6.4" "2.7.3
         "2.33.17" "2.33.18" "2.33.19" "2.33.20" "2.33.21" "2.33.22" "2.33.23" "2.33.24"
         "2.33.25" "2.33.26" "2.33.27" "2.33.28" "2.33.29" "2.33.30" "2.33.31" "2.34.0" "2.34.1"
         "2.34.2" "2.34.3" "2.34.4" "2.34.5" "2.34.6" "2.34.8" "2.34.9" "2.34.10" "2.34.11"
-        "2.34.12" "2.34.13" "2.34.14" {
+        "2.34.12" "2.34.13" "2.34.14" "2.34.15" {
   auto-version {
     git-tags = "https://github.com/aws/aws-cli.git"
   }
@@ -1092,4 +1092,7 @@ sha256sums = {
   "https://awscli.amazonaws.com/AWSCLIV2-2.34.14.pkg": "af0b09f2ec81380367f0336df90914a7c803132c3a90ac8cf0f7eaacceffa1c3",
   "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.34.14.zip": "05f820e8df6bdc2b1c296afd1b63d3fecd39ac0725fd823004f110191c8180dd",
   "https://awscli.amazonaws.com/awscli-exe-linux-aarch64-2.34.14.zip": "73e16cba45327e74540431bac0a9c7a3fe9c765d9ddf53a3c9415bd366c45159",
+  "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.34.15.zip": "76ebe0a6807f0b471d2ee8838ffef5c1cdb291b808a4fa51ca7758b7daf48eda",
+  "https://awscli.amazonaws.com/awscli-exe-linux-aarch64-2.34.15.zip": "a24566949bf64f5b13b9a58ca55a87f30febc77a3aaa14b0ca80629dd2ace523",
+  "https://awscli.amazonaws.com/AWSCLIV2-2.34.15.pkg": "b5823e3577033c1e8fa0420662ce4a5ee17560843d587c55484185317d90c434",
 }

buildkite-agent.hcl
@@ -16,7 +16,8 @@ version "3.33.3" "3.34.0" "3.34.1" "3.35.0" "3.35.1" "3.35.2" "3.36.0" "3.36.1"
         "3.102.2" "3.103.0" "3.103.1" "3.104.0" "3.105.0" "3.106.0" "3.107.0" "3.107.1"
         "3.107.2" "3.108.0" "3.109.0" "3.109.1" "3.110.0" "3.111.0" "3.112.0" "3.113.0"
         "3.114.0" "3.114.1" "3.115.0" "3.115.1" "3.115.2" "3.115.3" "3.115.4" "3.116.0"
-        "3.117.0" "3.118.0" "3.118.1" "3.119.0" "3.119.1" "3.119.2" "3.120.0" "3.120.1" {
+        "3.117.0" "3.118.0" "3.118.1" "3.119.0" "3.119.1" "3.119.2" "3.120.0" "3.120.1"
+        "3.120.3" {
   auto-version {
     github-release = "buildkite/agent"
   }
@@ -543,4 +544,8 @@ sha256sums = {
   "https://github.com/buildkite/agent/releases/download/v3.120.1/buildkite-agent-darwin-arm64-3.120.1.tar.gz": "785024e760071cb718230506dacdce218a32ea96617b2ea4784cd0a27352dba9",
   "https://github.com/buildkite/agent/releases/download/v3.120.1/buildkite-agent-linux-amd64-3.120.1.tar.gz": "73a182f73f192b4db1520470e9511bec878d48e0c0b5f4cd2cf85ae085926e91",
   "https://github.com/buildkite/agent/releases/download/v3.120.1/buildkite-agent-linux-arm64-3.120.1.tar.gz": "e58a9edbeb7c4febdf5c1c50112b22f37d30ee7791e0a689b80e63e276e370f0",
+  "https://github.com/buildkite/agent/releases/download/v3.120.3/buildkite-agent-linux-arm64-3.120.3.tar.gz": "3576bf9931081e16693b77d18005bbac9c8bbba89500c5253eaf783839ce9a66",
+  "https://github.com/buildkite/agent/releases/download/v3.120.3/buildkite-agent-darwin-arm64-3.120.3.tar.gz": "4c94ad62f61faad68d2c4defbfb454452779926027619bcadde737f9cb6e9d9a",
+  "https://github.com/buildkite/agent/releases/download/v3.120.3/buildkite-agent-darwin-amd64-3.120.3.tar.gz": "3753404db3c695ba7cf001a462846cdc62054ee97a3071814e7230647f94006c",
+  "https://github.com/buildkite/agent/releases/download/v3.120.3/buildkite-agent-linux-amd64-3.120.3.tar.gz": "fa60a2368d55b596f745e570de717df97954d2a0d85e93f899a551a39a0a9434",
 }

conftest.hcl
@@ -10,7 +10,7 @@ version "0.34.0" "0.35.0" "0.36.0" "0.37.0" "0.38.0" "0.39.0" "0.39.1" "0.39.2"
         "0.40.0" "0.41.0" "0.42.0" "0.42.1" "0.43.0" "0.43.1" "0.44.1" "0.45.0" "0.46.0"
         "0.47.0" "0.48.0" "0.49.0" "0.49.1" "0.50.0" "0.51.0" "0.52.0" "0.53.0" "0.54.0"
         "0.55.0" "0.56.0" "0.57.0" "0.58.0" "0.59.0" "0.60.0" "0.61.0" "0.61.1" "0.61.2"
-        "0.62.0" "0.63.0" "0.64.0" "0.65.0" "0.66.0" "0.67.0" {
+        "0.62.0" "0.63.0" "0.64.0" "0.65.0" "0.66.0" "0.67.0" "0.67.1" {
   auto-version {
     github-release = "open-policy-agent/conftest"
   }
@@ -181,4 +181,8 @@ sha256sums = {
   "https://github.com/open-policy-agent/conftest/releases/download/v0.67.0/conftest_0.67.0_darwin_arm64.tar.gz": "08fc2672ba56acab8d24ad9b7695340412446a48137af7fd8e61d8db187ad36b",
   "https://github.com/open-policy-agent/conftest/releases/download/v0.67.0/conftest_0.67.0_darwin_x86_64.tar.gz": "60d42a323918a9f3b17ce562f278974bd97a5a5b311a0145a60a2b2c78e867ba",
   "https://github.com/open-policy-agent/conftest/releases/download/v0.67.0/conftest_0.67.0_linux_arm64.tar.gz": "bdfd05e17ef899689d02bdf8b9dc1744d3da8053a952c5d67abd8a503881a54a",
+  "https://github.com/open-policy-agent/conftest/releases/download/v0.67.1/conftest_0.67.1_linux_x86_64.tar.gz": "dd99b76693507ac47cdf86ac1a3b177721671ad3db7c272a621bccb52795afac",
+  "https://github.com/open-policy-agent/conftest/releases/download/v0.67.1/conftest_0.67.1_linux_arm64.tar.gz": "5349fbdb80432f292256fd2065792b4842ec30b829a3c75cf22a36e66050164c",
+  "https://github.com/open-policy-agent/conftest/releases/download/v0.67.1/conftest_0.67.1_darwin_x86_64.tar.gz": "360a573f69e3dd0cef0806e3678f4d8e4c4bd31d69f1f52095494637db81312f",
+  "https://github.com/open-policy-agent/conftest/releases/download/v0.67.1/conftest_0.67.1_darwin_arm64.tar.gz": "62ba51198af54ddf9070b4cfb751fa6c06f55ef76c821e8037c629ad937bd324",
 }

copybara.hcl
@@ -17,7 +17,7 @@ version "20250512" "20250519" "20250526" "20250602" "20250609" "20250616" "20250
         "20250825" "20250901" "20250908" "20250915" "20250922" "20250929" "20251006" "20251013"
         "20251020" "20251027" "20251103" "20251110" "20251117" "20251124" "20251201" "20251208"
         "20251215" "20251222" "20251229" "20260105" "20260112" "20260119" "20260209" "20260216"
-        "20260223" "20260302" "20260309" "20260316" {
+        "20260223" "20260302" "20260309" "20260316" "20260323" {
   auto-version {
     github-release = "google/copybara"
   }

depot.hcl
@@ -10,7 +10,7 @@ version "2.84.2" "2.84.3" "2.84.4" "2.85.0" "2.85.1" "2.85.2" "2.85.3" "2.85.4"
         "2.96.1" "2.96.2" "2.97.1" "2.98.0" "2.99.0" "2.99.1" "2.100.1" "2.100.2" "2.100.5"
         "2.100.6" "2.100.8" "2.100.9" "2.100.11" "2.100.12" "2.100.13" "2.100.14" "2.101.0"
         "2.101.1" "2.101.3" "2.101.7" "2.101.8" "2.101.10" "2.101.11" "2.101.13" "2.101.14"
-        "2.101.15" "2.101.18" "2.101.19" "2.101.21" "2.101.26" "2.101.27" {
+        "2.101.15" "2.101.18" "2.101.19" "2.101.21" "2.101.26" "2.101.27" "2.101.29" {
   auto-version {
     github-release = "depot/cli"
   }
@@ -209,4 +209,8 @@ sha256sums = {
   "https://github.com/depot/cli/releases/download/v2.101.27/depot_2.101.27_darwin_amd64.tar.gz": "9bf5eb090121c6fe5fb6138c1d79ce1d8a10d4bb5fd07142edd0d76cbe0bd28b",
   "https://github.com/depot/cli/releases/download/v2.101.27/depot_2.101.27_darwin_arm64.tar.gz": "cc4dec346132ada56974f109a02ec4e115198ef365ff178b6a1f32174cefd48a",
   "https://github.com/depot/cli/releases/download/v2.101.27/depot_2.101.27_linux_arm64.tar.gz": "f9e3b12b7e777478a6e6a955200d71f7eeac98b9883dccecb70a578e6676264b",
+  "https://github.com/depot/cli/releases/download/v2.101.29/depot_2.101.29_darwin_arm64.tar.gz": "6180408510f759afe95ffc04b81e6f5b0ceffc0f454c50bbab4413171ae1da58",
+  "https://github.com/depot/cli/releases/download/v2.101.29/depot_2.101.29_darwin_amd64.tar.gz": "6b0aac73d9a78809a2f0e8e8f80a28df63357fdf459ed43f7214602ead085628",
+  "https://github.com/depot/cli/releases/download/v2.101.29/depot_2.101.29_linux_arm64.tar.gz": "162480b3e9a2d58a98401e07a379bdafa3d9937616878687fa198d5741f65444",
+  "https://github.com/depot/cli/releases/download/v2.101.29/depot_2.101.29_linux_amd64.tar.gz": "a397809681fc43db54448c9b3341a742d53c9e48befab349884672ab771ecb81",
 }

duckdb.hcl
@@ -22,7 +22,7 @@ version "0.4.0" {
 version "0.5.0" "0.5.1" "0.6.0" "0.6.1" "0.7.0" "0.7.1" "0.8.0" "0.8.1" "0.9.0" "0.9.1"
         "0.9.2" "0.10.0" "0.10.1" "0.10.2" "0.10.3" "1.0.0" "1.1.0" "1.1.1" "1.1.2" "1.1.3"
         "1.2.0" "1.2.1" "1.2.2" "1.3.0" "1.3.1" "1.3.2" "1.4.0" "1.4.1" "1.4.2" "1.4.3" "1.4.4"
-        "1.5.0" {
+        "1.5.0" "1.5.1" {
   auto-version {
     github-release = "duckdb/duckdb"
   }
@@ -118,4 +118,6 @@ sha256sums = {
   "https://github.com/duckdb/duckdb/releases/download/v1.4.4/duckdb_cli-linux-amd64.zip": "ea79eae4233f1aba9a020c8a61877de38a789bc62cdd37485d3589cd77dc0d3e",
   "https://github.com/duckdb/duckdb/releases/download/v1.5.0/duckdb_cli-linux-amd64.zip": "179a481edf048dd1fef0f097d66933b4b5f5a5737d4034c645e415ec722c0292",
   "https://github.com/duckdb/duckdb/releases/download/v1.5.0/duckdb_cli-osx-universal.zip": "108e7a62e42286a0b6d88f49e1a792907f85c70d0ead350cc3cb69e59124038b",
+  "https://github.com/duckdb/duckdb/releases/download/v1.5.1/duckdb_cli-linux-amd64.zip": "7414b29d4e77ae833ff170a489a59b74331f96d3fde72aa3bb8437dedbcea180",
+  "https://github.com/duckdb/duckdb/releases/download/v1.5.1/duckdb_cli-osx-universal.zip": "fa6bddb0e1ce80cb0ea40201b8f5fc1a28693b2a2aec3295ed4cfc41b770485d",
 }

commit eb538053c7f8309f6dba6f4b292b487dd6c823ec

zellij: fix sha256-source url and wrong checksum (#742)
Root cause: the linux package block was trying to download the
zellij-no-web-... tarball, but its sha256-source was mistakenly pointing
to the standard zellij-... .sha256sum file. This caused hermit to
populate the sha256sums block with the checksum of the standard version
(d7239...) instead of the no-web version (458b0...).

Started failing after:
https://github.com/cashapp/hermit-packages/commit/b4ca316e5742acbc778245a43812a4e7df787e1f

zellij.hcl
@@ -4,7 +4,7 @@ test = "zellij --version"
 
 linux {
   source = "https://github.com/zellij-org/zellij/releases/download/v${version}/zellij-no-web-${xarch}-unknown-linux-musl.tar.gz"
-  sha256-source = "https://github.com/zellij-org/zellij/releases/download/v${version}/zellij-${xarch}-unknown-linux-musl.sha256sum"
+  sha256-source = "https://github.com/zellij-org/zellij/releases/download/v${version}/zellij-no-web-${xarch}-unknown-linux-musl.sha256sum"
 }
 
 darwin {
@@ -23,7 +23,7 @@ sha256sums = {
   "https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-x86_64-apple-darwin.tar.gz": "7b0c1a9c2591eadf506ec58d62ef5f6d9c93d089a3603142af3dcca5fa575d44",
   "https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-aarch64-apple-darwin.tar.gz": "a09ea51f3d98427253de2b889bb04f1aa0850fbb034911c7a01b8f0194edba66",
   "https://github.com/zellij-org/zellij/releases/download/v0.43.1/zellij-no-web-aarch64-unknown-linux-musl.tar.gz": "8ced877df27a8fe9112607dd3d772442aefa5e42359cda1baba53e78c4ae46aa",
-  "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-no-web-x86_64-unknown-linux-musl.tar.gz": "d7239c8f8c08dc7bb73920fa7757d776a81f899a45edfc1d0c862a0368db7127",
+  "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-no-web-x86_64-unknown-linux-musl.tar.gz": "458b0c5ec19d6313580293e451c9a467c73b337d42faf8e2ce1712c56767b727",
   "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-x86_64-apple-darwin.tar.gz": "aa95a65c99ac9b9e609411ca0897f3778d2cd8a8363c71b61b68101758532b14",
   "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-aarch64-apple-darwin.tar.gz": "89a9273955c64bfafa1325e227a6c3bb3b81c2648b5999a69d57f6728933b1b8",
   "https://github.com/zellij-org/zellij/releases/download/v0.44.0/zellij-no-web-aarch64-unknown-linux-musl.tar.gz": "4b2d47a210dcaabde40e659bfb00677d2dccce2238e562a145fe8df68a35b6c4",